598ba7e4fb6e7a6196f30f882891c4c27bdc5215
Berthland Berto <bberto21@gmail.com>
2026-03-17 10:26:37 -0400

    Initial Commit

---
+++ HourPulse/HourPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourPulseApp.swift
+//  HourPulse
+//
+//  Created by Admin on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct HourPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
